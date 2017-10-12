ef70d7eb726f824ee003011b68dd46f0e4bfcfcc
jlb <497012571@qq.com>
2017-10-12 11:01:28 +0800

    新增功能,readme.md

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 ## sublime text 3的php插件
 ### 适用php框架Thinkphp3
 
+### 更新日志
+> 2017年10月11日
+
+1.支持直接路由匹配视图
+2.新增匹配方法中的参数
+
 ### 安装
 
 1. 打开
@@ -9,36 +15,28 @@
 Preferences->Browsswer Packages
 
 ```
-
 2. 新建一个文件夹TpRoute
-
-3. 项目的所有文件复制进TpRoute文件夹即可
-
+3. 项目的所有文件复制进TpRoute文件夹
 
 ### 用处介绍
 
 解决频繁手工输入url地址来调试功能,根据插件的配置的路由规则快速打开指定方法
 
-
 > 插件设置
-
 
 ```
 设置
 Preferences->Package Settings->TpRoute->Settings - User
-
-
-// 输入
-
 {
 	"tp_host" : "http://localhost/index.php?m={m}&a={a}&c={c}",
 }
-
-
 ```
 
 > {m}模块，{c}控制器，{a}方法
 
+### 快捷键
+
+> F9
 
 ### 效果
 
--- TpRoute.py
+++ TpRoute.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 import sublime
 import sublime_plugin
 import webbrowser
@@ -10,47 +11,89 @@ PACKAGE_NAME = 'TpRoute'
 
 
 class TpRouteCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		settings = self.getSettings()
-		filename = self.view.file_name()
 
-		host = ''
-		if settings.get('tp_host'):
-			host = settings.get('tp_host')
+	def run(self, edit):
+		self.settings = self.getSettings()
+		self.host = self.settings.get('tp_host')
+		ext = os.path.splitext(self.view.file_name())[1]
 
 		self.view.run_command('save')
 
-		# 获取当前文件里的所有方法名
-		actions = []
-		with open(filename, 'r',encoding = 'utf-8') as f:
-			text = f.read()
-			# 匹配方法名
-			m = re.findall(r'function\s+([\w\d_]+)',text)
-			if m :
-				actions = m
+		if ext == '.html':
+			self.openInView()
 
-		moduleName = self.getTpModuleName(filename)
-		controllerName = os.path.basename(filename).replace('Controller.class.php','').replace('.php','')
-
-		for sel in self.view.sel():
-			# 获取当前选中文字
-			action = self.view.substr(sel)
-			if action in actions:
-				actionName = actions[actions.index(action)]
-
-				host = host.replace('{m}', moduleName)
-				host = host.replace('{c}', controllerName)
-				host = host.replace('{a}', actionName)
-
-				webbrowser.open(host)
+		if ext == '.php':
+			self.openInController()
 
 	def getSettings(self):
 		return sublime.load_settings(PACKAGE_NAME + '.sublime-settings')
 
 
-	def getTpModuleName(self, filename,level = 3,dir_name=''):
+	def getParentDir(self, filename,level = 3,dir_name=''):
 		if level == 0:
 			return dir_name
 
 		path,__dirname = os.path.split(filename)
-		return self.getTpModuleName(path,level-1,__dirname)
+		return self.getParentDir(path,level-1,__dirname)
+
+
+	def openInView(self):
+		moduleName = self.getParentDir(self.view.file_name(),4)
+		controllerName = self.getParentDir(self.view.file_name(),2)
+		action = self.getParentDir(self.view.file_name(),1).replace('.html','')
+
+		self.host = self.host.replace('{m}', moduleName)
+		self.host = self.host.replace('{c}', controllerName)
+		self.host = self.host.replace('{a}', action)
+		webbrowser.open(self.host)
+
+	def openInController(self):
+		# 解析类中方法
+		actions = {}
+		with open(self.view.file_name(), 'r',encoding = 'utf-8') as f:
+			text = f.read()
+			# 解析方法与参数
+			m = re.findall(r'function\s+([\w\d_]+)\s*?\(([$\w\d_,="\']*?)\)',text)
+			for action,param in m:
+				actions[action] = self.parseParam(param)
+
+		moduleName = self.getParentDir(self.view.file_name())
+		controllerName = os.path.basename(self.view.file_name()).replace('Controller.class.php','').replace('.php','')
+		
+		for sel in self.view.sel():
+			# 获取当前选中文字
+			action = self.view.substr(sel)
+			if action in actions.keys():
+				param = actions[action]
+				self.host = self.host.replace('{m}', moduleName)
+				self.host = self.host.replace('{c}', controllerName)
+				self.host = self.host.replace('{a}', action)
+				webbrowser.open(self.host + '&' + param)
+
+
+	# 解析参数
+	def parseParam(self,paramStr):
+		if not paramStr:
+			return ''
+		param = ''
+
+		for s in paramStr.replace('$','').split(','):
+			if len(s.split('=')) == 1:
+				param += (s + '=&')
+
+			if len(s.split('=')) == 2:
+				k,v = s.split('=')
+				param += ("%s=%s&" % (k,v))
+
+		return param.rstrip('&')
+
+
+
+
+
+
+
+
+
+
+
